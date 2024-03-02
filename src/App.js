9e502c91b05511d36184f8ae0e5a70be555dd356
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2024-03-02 22:08:21 +0600

    switch case

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Component/Header';
 import Hero from './Component/Hero';
+import Footer from './Component/Footer';
 
 const App = () => {
   return (
     <div>
     <Header/>
     <Hero/>
+    <Footer/>
     
     </div>
   );
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+   const status = false;
+   switch(status){
+    case true:
+        return <button>logout</button>
+        case false:
+        return <button>login</button>
+        default:
+            return null;
+
+   }
+
 };
 
 export default Footer;
